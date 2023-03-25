dd9e5d9a14cb73c61cf54bc0878c1ceccff98433
Andrei Padureanu <andrei.padureanu@atta.systems>
2023-03-25 08:59:18 +0200

    Initial commit

---
+++ src/test/java/com/mytones/core/MytonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytones.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytonesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
